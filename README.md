38476116bb5423407e44356c5ab3ca1a729742fd
Patricio López Juri <lopezjuripatricio@gmail.com>
2021-02-20 12:04:36 -0300

    feat: Initial version

--- README.md
+++ README.md
@@ -1,27 +1,44 @@
 # use-next-intl-format
 
-> React Hook for creating instance of the native Intl library using Next.js i18n
+> React Hook for creating memoized native instances of Intl for Next.js i18n
 
 [![NPM](https://img.shields.io/npm/v/use-next-intl-format.svg)](https://www.npmjs.com/package/use-next-intl-format) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
 ## Install
 
+This module includes Typescript typings.
+
 ```bash
 npm install --save use-next-intl-format
+
+yarn add use-next-intl-format
 ```
 
 ## Usage
 
+> See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat#using_options for `options` reference
+
+> See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat#using_options for `options` reference
+
+**This only applies for Next.js apps.**
+
 ```tsx
 import * as React from 'react'
 
-import { useMyHook } from 'use-next-intl-format'
+import { useIntlDateTimeFormat, useIntlNumberFormat } from 'use-next-intl-format'
 
 const Example = () => {
-  const example = useMyHook()
+  const dateFormatter = useIntlDateTimeFormat({
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  })
+  const numberFormatter = useIntlNumberFormat({ notation: "compact" })
+
   return (
     <div>
-      {example}
+      Current datetime: {dateFormatter.format(new Date())}
+      Number: {numberFormatter.format(2)}
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,33 @@
-import * as React from 'react';
+import { useMemo } from "react";
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState<{
-    counter: number;
-  }>({
-    counter: 0
-  });
+import { useRouter } from "next/router";
 
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++;
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval);
-    };
-  }, []);
+/**
+ * Create memoized Intl.DateTimeFormat instance based on current locale
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat
+ */
+export function useIntlDateTimeFormat(options: Intl.DateTimeFormatOptions = {}): Intl.DateTimeFormat {
+  const router = useRouter();
 
-  return counter;
-};
+  const deps = Object.values(options); // they are all primitives
+  const formatter = useMemo(() => {
+    return new Intl.DateTimeFormat(router.locale || "default", options);
+  }, [router.locale, deps, deps.length]);
+
+  return formatter;
+}
+
+/**
+ * Create memoized Intl.NumberFormat instance based on current locale
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat
+ */
+export function useIntlNumberFormat(options: Intl.NumberFormatOptions = {}): Intl.NumberFormat {
+  const router = useRouter();
+
+  const deps = Object.values(options); // they are all primitives
+  const formatter = useMemo(() => {
+    return new Intl.NumberFormat(router.locale || "default", options);
+  }, [router.locale, deps, deps.length]);
+
+  return formatter;
+}
